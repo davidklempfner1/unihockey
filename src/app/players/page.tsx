54c0df84aa605c25d960bdfce97b46d2ea6896f7
davidklempfner2 <david.klempfner@cashrewards.com>
2025-04-23 17:42:11 +1000

    Turned players from server to client component.

--- src/app/players/page.tsx
+++ src/app/players/page.tsx
@@ -1,10 +1,10 @@
-// 'use client';
+'use client';
 
 export default function Page() {
     return (
         <>
             <h1>The Players Page</h1>
-            {/* <button onClick={() => alert('Hello from /players')}>Click me!</button> */}
+            <button onClick={() => alert('Hello from /players')}>Click me!</button>
         </>
     );
 }
